ab2eab30c935e9cf55a0abce0a5107dc0e5fe973
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-01-10 22:02:38 +0100

    Delete some unnecessary things

--- src/com/mrozwadowski/checkers/game/Game.java
+++ src/com/mrozwadowski/checkers/game/Game.java
@@ -1,10 +1,8 @@
 package com.mrozwadowski.checkers.game;
 
-import com.mrozwadowski.checkers.errors.IllegalMoveException;
 import com.mrozwadowski.checkers.events.GameEventListener;
 import com.mrozwadowski.checkers.players.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
@@ -147,83 +145,6 @@ public class Game {
         listener.pawnMoved(pawn, move);
         switchTurn();
     }
-
-    /*
-    public void move(Field source, Field target) throws IllegalMoveException {
-        if (!source.hasPawn()) {
-            throw new IllegalMoveException("Source field must have a pawn on it!");
-        }
-        Pawn pawn = source.getPawn();
-        if (pawn.getColor() != turn) {
-            throw new IllegalMoveException("It's not your turn!");
-        }
-        if (target.hasPawn()) {
-            throw new IllegalMoveException("Target field must be empty!");
-        }
-
-        int i0 = source.getRow();
-        int j0 = source.getColumn();
-        int i = target.getRow();
-        int j = target.getColumn();
-
-
-        int di = i - i0;
-        int dj = j - j0;
-        if (Math.abs(di) != Math.abs(dj)) {
-            throw new IllegalMoveException("Moves must be diagonal!");
-        }
-        if (pawn.isCrowned()) {
-            int iStep = di / Math.abs(di);
-            int jStep = dj / Math.abs(dj);
-            int iBetween = i0 + iStep;
-            int jBetween = j0 + jStep;
-            for (int k=1; k<Math.abs(di); k++) {
-                Field between = board.getFieldAt(iBetween, jBetween);
-
-                if (between.hasPawn()) {
-                    Pawn captured = between.getPawn();
-                    if (captured.getColor() == pawn.getColor()) {
-                        throw new IllegalMoveException("Invalid capture!");
-                    } else {
-                        // listener.pawnCaptured(captured);
-                        between.setPawn(null);
-                    }
-                }
-
-                iBetween += iStep;
-                jBetween += jStep;
-            };
-        } else {
-            if (Math.abs(di) == 2) {
-                int iBetween = (target.getRow() + source.getRow()) / 2;
-                int jBetween = (target.getColumn() + source.getColumn()) / 2;
-                Field between = board.getFieldAt(iBetween, jBetween);
-                Pawn captured = between.getPawn();
-
-                if (captured != null && pawn.getColor() != captured.getColor()) {
-                    // listener.pawnCaptured(captured);
-                    between.setPawn(null);
-                } else {
-                    throw new IllegalMoveException("Invalid capture!");
-                }
-            } else {
-                if (di > 0 && pawn.isBlack() || di < 0 && !pawn.isBlack()) {
-                    throw new IllegalMoveException("No backward moves!");
-                }
-            }
-        }
-
-        if (pawn.isBlack() && i == 0 || !pawn.isBlack() && i == board.getSize() - 1) {
-            pawn.setCrowned(true);
-            listener.pawnCrowned(pawn);
-        }
-
-        pawn.moveTo(target);
-        listener.pawnMoved(pawn, new MoveSequence(source, target));
-
-        switchTurn();
-    }
-    */
 
     private void switchTurn() {
         List<MoveSequence> moves;
